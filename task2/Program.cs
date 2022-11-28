df9857e708c41635259567bd385f8f28387619e1
unknown <facturs@mail.com>
2022-11-28 21:32:23 +0700

    homework2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>99)
+{int div1 = num % 10;
+Console.Write($"{div1} ");}
+else
+{
+Console.Write("третьего числа нет");
+}
+
